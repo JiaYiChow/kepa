7e65ef7b3e9ccd6afe1b056bb81087c6b028d252
Chow Jia Yi <jcjiayichow@gmail.com>
2020-05-15 21:29:56 +0800

    Implemented changing of octaves and major

--- app.js
+++ app.js
@@ -1,52 +1,47 @@
-/**some app to play pipa sounds on the keyboard 
- * 1. play sounds upon pressing key (yay functionality done)
- * 2. change key 
- * 3. tremolo 
-*/
-const major = "A"; 
+var major = "A"; 
 const default_note = 16; 
 const num_to_note = {
     1: "A2", 
-    2: "A#2", 
+    2: "A2_sharp", 
     3: "B2", 
     4: "C3", 
-    5: "C#3", 
+    5: "C3_sharp", 
     6: "D3", 
-    7: "D#3", 
+    7: "D3_sharp", 
     8: "E3", 
     9: "F3", 
-    10: "F#3", 
+    10: "F3_sharp", 
     11: "G3", 
-    12: "G#3", 
+    12: "G3_sharp", 
     13: "A3", 
-    14: "A#3", 
+    14: "A3_sharp", 
     15: "B3", 
     16: "C4", 
-    17: "C#4", 
+    17: "C4_sharp", 
     18: "D4", 
-    19: "D#4", 
+    19: "D4_sharp", 
     20: "E4", 
     21: "F4", 
-    22: "F#4", 
+    22: "F4_sharp", 
     23: "G4", 
-    24: "G#4", 
+    24: "G4_sharp", 
     25: "A4", 
-    26: "A#4", 
+    26: "A4_sharp", 
     27: "B4", 
     28: "C5", 
-    29: "C#5", 
+    29: "C5_sharp", 
     30: "D5",  
-    31: "D#5", 
+    31: "D5_sharp", 
     32: "E5", 
     33: "F5", 
-    34: "F#5", 
+    34: "F5_sharp", 
     35: "G5", 
-    36: "G#5", 
+    36: "G5_sharp", 
     37: "A5", 
-    38: "A#5", 
+    38: "A5_sharp", 
     39: "B5", 
     40: "C6", 
-    41: "C#6", 
+    41: "C6_sharp", 
     42: "D6", 
     43: "E6"
 }
@@ -54,7 +49,7 @@ const num_to_note = {
 const major_list = ["A", "Bb", "C", "D", "Eb", "E", "F", "G"]; 
 
 const major_to_first_note = {
-    "A": 1, 
+    "A": 25, 
     "Bb": 26, 
     "C": 16, 
     "D": 18, 
@@ -64,33 +59,46 @@ const major_to_first_note = {
     "G": 23, 
 }
 
-var first_note = major_to_first_note[major]; 
+var first_note;
+first_note = major_to_first_note[major]; 
 
 function raise_octave(first_note) {
-    if (first_note + 12 <= 19){
-        return first_note + 12;
+    if (first_note + 12 <= 43){
+        first_note += 12
+        console.log("new first note:" + first_note);
     } else {
         alert("Reached limit!");
     } 
+    return first_note;
 }
 
 function decrease_octave(first_note){ 
-    if (first_note - 12 >= 13){
-        return first_note - 12
+    if (first_note - 12 >= 1){
+        first_note -= 12;  
     } else {
         alert("Cannot decrease anymore!"); 
     }
+    return first_note;
 }
 
 function increase_key(major){
     let index = major_list.indexOf(major); 
     index = (index + 1) % major_list.length; 
-    return major_list[index]; 
+    let new_major = major_list[index]; 
+    first_note = major_to_first_note[new_major];
+    console.log("Index:" + index); 
+    console.log("New major:" + new_major);
+    console.log("New first_note:" + first_note);
+    return new_major; 
 }
 
 function decrease_key(major){
     let index = major_list.indexOf(major); 
-    index = (index - 1) % major_list.length; 
+    index = ((index - 1) % (major_list.length)) + major_list;
+    console.log("Index:" + index);
+    let new_major = major_list[index];
+    first_note = major_to_first_note[new_major]; 
+    return new_major;
 }
 
 //program the notes into the keyboard 
@@ -102,88 +110,124 @@ function keyPressed(e){
         //doh 
         case "KeyQ": 
             note = first_note;
+            playNote(note);
             break; 
         //doh sharp 
         case "Digit2": 
             note = first_note + 1; 
+            playNote(note);
             break; 
         //rei 
         case "KeyW": 
             note = first_note + 2; 
+            playNote(note);
             break;
         //rei sharp 
         case "Digit3":
             note = first_note + 3; 
+            playNote(note);
             break; 
         //mi 
         case "KeyE": 
             note = first_note + 4; 
+            playNote(note);
             break;
         //fa 
         case "KeyR": 
             note = first_note + 5; 
+            playNote(note);
             break; 
         //fa sharp 
         case "Digit5":
             note = first_note + 6;
+            playNote(note);
             break; 
         //soh
         case "KeyT": 
             note = first_note + 7; 
+            playNote(note);
             break; 
         //soh sharp 
         case "Digit6":
             note = first_note + 8;
+            playNote(note);
             break; 
         //la 
         case "KeyY":
             note = first_note + 9; 
+            playNote(note);
             break; 
         //la sharp 
         case "Digit7": 
             note = first_note + 10; 
+            playNote(note);
             break; 
         //ti 
         case "KeyU":
             note = first_note + 11; 
+            playNote(note);
             break; 
         //doh (higher octave)
         case "KeyI":
             note = first_note + 12; 
+            playNote(note);
             break; 
         //doh sharp (higher octave)
         case "Digit9":
             note = first_note + 13; 
+            playNote(note);
             break; 
         //rei (higher octave)
         case "KeyO": 
             note = first_note + 14; 
+            playNote(note);
             break;
         //rei sharp (higher octave)
-        case "Key0":
+        case "Digit0":
             note = first_note + 15; 
+            playNote(note);
             break;  
         //mi (higher octave)
         case "KeyP": 
             note = first_note + 16; 
+            playNote(note);
+            break; 
+        //increase octave
+        case "ArrowUp":
+            first_note = raise_octave(first_note);
+            break; 
+        //decrease octave 
+        case "ArrowDown":
+            first_note = decrease_octave(first_note);
+            break;
+        //increase key 
+        case "ArrowRight":
+            major = increase_key(major);
+            break; 
+        case "ArrowLeft":
+            major = decrease_key(major); 
+            break; 
+        default: 
             break; 
     }   
-    return note; 
 }
 
-function playNote(e){
+function playNote(note_num){
     var myMusic = new Audio();
     //get the number reprensentative of the note 
-    let note = keyPressed(e); 
-    console.log("Note number:" + note);
+    console.log("Note number:" + note_num);
     //find the note from the number 
-    let note_name = num_to_note[note];
+    let note_name = num_to_note[note_num];
     console.log("Note name:" + note_name);
     myMusic.src = "./sounds/" + note_name + ".m4a"
     myMusic.play(); 
 }
 
+
+
 window.onload = () => {    
-    //KeyListener 
-    document.addEventListener('keydown', playNote); 
+    //KeyListener for notes 
+    document.addEventListener('keydown', keyPressed); 
+    //KeyListener for octave/key change 
+    //document.addEventListener('keydown', changeOctave);
 }
