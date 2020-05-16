7a4925915b1ccffb7df306e0d81e1812b8745604
Chow Jia Yi <jcjiayichow@gmail.com>
2020-05-16 16:19:40 +0800

    Refactored code and fixed some bugs

--- README.md
+++ README.md
@@ -3,27 +3,23 @@ With the KePa, you can now play the pipa without having to purchase one expensiv
 
 This program is written in javascript, and I realise I have to host it in a server of some sorts or run it in a webpage... Would be appreciated if someone can give me some ideas as to how I can create my program (or maybe give me suggestions for a better programming language to use, or suggestions in general).
 
-##Using the KePa 
+## Using the KePa 
 For now you have to download this and drag the index.html page to use it. 
 The diagram below shows how to use it, the black represents your keyboard and the red represents the note/function: 
-![alt text](./images/KePa diagram.pdf)
+![KePa diagram] (./images/KePa diagram.pdf)
 
 Most probably gonna create a website where this thing works. (and maybe get some ad revenue yay)
 
 Stay tuned! 
 
-##Progress 
+## Progress 
 1. You can play the notes now. 
+2. Change octave by pressing up and down buttons. 
+3. Change key by pressing left and right buttons. 
 
-##Future plans 
-1. Functionality to change octave. 
-2. Functionality to change major. 
-3. Functionality to record sounds :DDDDD
-4. Including tremolo. (There is more than 1 way to do a tremolo, and each tremolo has different effects haha)
-5. Perhaps some other miscallnaeous techniques of the pipa because the pipa is awesome（扫弦，绞弦） :D
+## Future plans 
+1. Functionality to record sounds :DDDDD (and maybe download)
+2. Including tremolo. (There is more than 1 way to do a tremolo, and each tremolo has different effects haha)
+3. Perhaps some other miscallnaeous techniques of the pipa because the pipa is awesome（扫弦，绞弦） :D
 
-##DEBUG 
-1. Pressing any random key triggers the keyPressed function(done)
-2. decreasing major is buggy 
-3. chaning major and then changing octave is kinda weird 
 
--- app.js
+++ app.js
@@ -1,110 +1,87 @@
-var major = "A"; 
-const default_note = 16; 
-const num_to_note = {
-    1: "A2", 
-    2: "A2_sharp", 
-    3: "B2", 
-    4: "C3", 
-    5: "C3_sharp", 
-    6: "D3", 
-    7: "D3_sharp", 
-    8: "E3", 
-    9: "F3", 
-    10: "F3_sharp", 
-    11: "G3", 
-    12: "G3_sharp", 
-    13: "A3", 
-    14: "A3_sharp", 
-    15: "B3", 
-    16: "C4", 
-    17: "C4_sharp", 
-    18: "D4", 
-    19: "D4_sharp", 
-    20: "E4", 
-    21: "F4", 
-    22: "F4_sharp", 
-    23: "G4", 
-    24: "G4_sharp", 
-    25: "A4", 
-    26: "A4_sharp", 
-    27: "B4", 
-    28: "C5", 
-    29: "C5_sharp", 
-    30: "D5",  
-    31: "D5_sharp", 
-    32: "E5", 
-    33: "F5", 
-    34: "F5_sharp", 
-    35: "G5", 
-    36: "G5_sharp", 
-    37: "A5", 
-    38: "A5_sharp", 
-    39: "B5", 
-    40: "C6", 
-    41: "C6_sharp", 
-    42: "D6", 
-    43: "E6"
+//declare constants 
+const DEFAULT_OCTAVE = 4; 
+const DEFAULT_MAJOR = "C"; 
+const DEFAULT_NOTE = 15; 
+const NOTE_LIST = [ 
+    "A2", "Bb2", "B2", "C3", "Db3", "D3", "Eb3", "E3", "F3", 
+    "Gb3", "G3", "Ab3", "A3", "Bb3", "B3", "C4", "Db4", "D4", 
+    "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4", "B4", 
+    "C5", "Db5", "D5", "Eb5", "E5", "F5", "Gb5", "G5", "Ab5", 
+    "A5", "Bb5", "B5", "C6", "Db6", "D6", "E6" 
+]
+const MAJOR_LIST = ["A", "Bb", "C", "D", "Eb", "E", "F", "G"]; 
+
+var major = DEFAULT_MAJOR; 
+var octave = DEFAULT_OCTAVE;  
+var first_note = DEFAULT_NOTE;
+
+/** Sets first_note according to global variables octave and 
+ *  major. 
+ *  Takes in no params and returns None. 
+*/
+function setFirstNote(){
+    console.log("setFirstNote()"); 
+    let string = major + octave; 
+    first_note = NOTE_LIST.indexOf(string); 
+    console.log("First_note: " + first_note); 
+    return; 
 }
 
-const major_list = ["A", "Bb", "C", "D", "Eb", "E", "F", "G"]; 
-
-const major_to_first_note = {
-    "A": 25, 
-    "Bb": 26, 
-    "C": 16, 
-    "D": 18, 
-    "Eb": 19, 
-    "E": 20, 
-    "F": 21, 
-    "G": 23, 
-}
-
-var first_note;
-first_note = major_to_first_note[major]; 
-
-function raise_octave(first_note) {
-    if (first_note + 12 <= 43){
-        first_note += 12
-        console.log("new first note:" + first_note);
+/** Increments the global variable octave. Cannot go beyond 
+ *  6 or else will have alert. Also updates first_note. Returns None.  
+ */
+function raise_octave() {
+    if (octave < 6){
+        octave++; 
+        setFirstNote(); 
+        console.log("Octave: " + octave); 
     } else {
-        alert("Reached limit!");
-    } 
-    return first_note;
-}
-
-function decrease_octave(first_note){ 
-    if (first_note - 12 >= 1){
-        first_note -= 12;  
-    } else {
-        alert("Cannot decrease anymore!"); 
+        alert("Cannot increase anymore!");
     }
-    return first_note;
+}
+/** Decrements the global variable octave. Cannot go below 2 
+ *  or else will have alert. Also updates first_note. Returns None. 
+ */
+function decrease_octave(){ 
+    if (octave > 2){
+        octave--; 
+        setFirstNote();
+        console.log("Octave: " + octave);
+    } else {
+        alert("Cannot decrease anymore!");
+    }
 }
 
-function increase_key(major){
-    let index = major_list.indexOf(major); 
-    index = (index + 1) % major_list.length; 
-    let new_major = major_list[index]; 
-    first_note = major_to_first_note[new_major];
-    console.log("Index:" + index); 
-    console.log("New major:" + new_major);
-    console.log("New first_note:" + first_note);
-    return new_major; 
+/** Increases key. It changes the global variable major. 
+ *  Updates first_note as well. Returns None. 
+ */
+function increase_key(){
+    let index = MAJOR_LIST.indexOf(major);
+    index = (index + 1) % MAJOR_LIST.length; 
+    major = MAJOR_LIST[index];
+    setFirstNote(); 
+    console.log("Major: " + major); 
+    return; 
 }
 
-function decrease_key(major){
-    let index = major_list.indexOf(major); 
-    index = (((index - 1 + major_list.length) % (major_list.length)));
-    let new_major = major_list[index];
-    first_note = major_to_first_note[new_major];
-    console.log("Index:" + index + "\n"); 
-    console.log("New major:" + new_major);
-    console.log("New first_note:" + first_note);
-    return new_major;
+/** Decreases key. It changes the global variable major. 
+ *  Updates first_note as well. Returns None. 
+ */
+function decrease_key(){
+    let index = MAJOR_LIST.indexOf(major); 
+    console.log("Old index: " + index);
+    index = (index - 1 + MAJOR_LIST.length) % (MAJOR_LIST.length);
+    console.log("new index: " + index); 
+    major = MAJOR_LIST[index];
+    setFirstNote();
+    console.log("Major: " + major); 
+    return; 
 }
 
-//program the notes into the keyboard 
-/** Q is 1, and so on, P is 2 of the higher octave. Returns an integer which maps to a note according to num_to_note property */
+/** Programs all the notes and implemented features into the keyboard. 
+ *  Q is 1, and so on, P is 3 of the higher octave. For more 
+ *  details refer to the diagram. 
+ */
 //ok i probably need to check for out of range for this 
 function keyPressed(e){
     let note; 
@@ -196,18 +173,18 @@ function keyPressed(e){
             break; 
         //increase octave
         case "ArrowUp":
-            first_note = raise_octave(first_note);
+            raise_octave();
             break; 
         //decrease octave 
         case "ArrowDown":
-            first_note = decrease_octave(first_note);
+            decrease_octave();
             break;
         //increase key 
         case "ArrowRight":
-            major = increase_key(major);
+            increase_key();
             break; 
         case "ArrowLeft":
-            major = decrease_key(major); 
+            decrease_key(); 
             break; 
         default: 
             break; 
@@ -219,17 +196,13 @@ function playNote(note_num){
     //get the number reprensentative of the note 
     console.log("Note number:" + note_num);
     //find the note from the number 
-    let note_name = num_to_note[note_num];
+    let note_name = NOTE_LIST[note_num];
     console.log("Note name:" + note_name);
     myMusic.src = "./sounds/" + note_name + ".m4a"
     myMusic.play(); 
 }
 
-
-
 window.onload = () => {    
     //KeyListener for notes 
     document.addEventListener('keydown', keyPressed); 
-    //KeyListener for octave/key change 
-    //document.addEventListener('keydown', changeOctave);
 }
